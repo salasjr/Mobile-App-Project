b40f72925c274be8748cfa25fba76f7a6b05715d
fikreanteneh <105011492+fikreanteneh@users.noreply.github.com>
2023-05-26 17:53:44 +0300

    Updated main with to make it modular

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,72 +1,33 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'package:trad_doctors/constants/global_variables.dart';
-import 'package:trad_doctors/features/auth/services/auth_service.dart';
-import 'package:trad_doctors/providers/all_doctors_provider.dart';
-import 'package:trad_doctors/router.dart';
-import 'common/doctor_bottom_bar.dart';
-import 'common/patient_bottom_bar.dart';
-import 'features/landing/screens/landing_screen.dart';
-import 'features/loading/loading_screen.dart';
-import 'providers/user_provider.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:trad_doctors_new/presentation/pages/home/doctor_home/screen/doctor_home.dart';
+import 'application/bloc/home_bloc/home_bloc.dart';
 
 void main() {
-  runApp(MultiProvider(providers: [
-    ChangeNotifierProvider(
-      create: (context) => UserProvider(),
-    ),
-    ChangeNotifierProvider(
-      create: (context) => AllDoctorsProvider(),
-    )
-  ], child: const MyApp()));
+  runApp(const MyApp());
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  AuthService authService = AuthService();
-  bool _isLoading = true;
-
-  @override
-  void initState() {
-    super.initState();
-    authService.getUserData(context).then((userInfo) {
-      setState(() {
-        _isLoading = false;
-      });
-    });
-  }
-
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Trad-Doctors',
+      title: 'Flutter Demo',
       theme: ThemeData(
-        scaffoldBackgroundColor: GlobalVariables.backgroundColor,
-        colorScheme:
-            const ColorScheme.light(primary: GlobalVariables.primaryColor),
-        appBarTheme: const AppBarTheme(
-          elevation: 0,
-          iconTheme: IconThemeData(
-            color: Colors.black,
-          ),
-        ),
+        primarySwatch: Colors.blue,
       ),
-      onGenerateRoute: ((settings) => generateRoute(settings)),
-      // home: const BottomBar());
-      home: _isLoading
-          ? const LoadingScreen()
-          : Provider.of<UserProvider>(context).user.token.isNotEmpty
-              ? Provider.of<UserProvider>(context).user.role == "doctor"
-                  ? const DoctorBottomBar()
-                  : const PatientBottomBar()
-              : const LandingPage(),
+      home: const HomePage(),
     );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return DoctorBottomBar();
   }
 }
