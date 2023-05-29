cb669912b748b378ca51be9e05a8c3c5155b7972
Surafel Workayehu <surafelwork32@gmail.com>
2023-05-29 14:12:39 +0300

    created patient_appoinetment_bloc_event.dart

---
+++ trad_doctors_new/lib/application/bloc/patient_appointement_bloc/patient_appoinetment_bloc_event.dart
@@ -0,0 +1,8 @@
+part of 'patient_appoinetment_bloc.dart';
+
+abstract class PatientAppoinetmentBlocEvent extends Equatable {
+  const PatientAppoinetmentBlocEvent();
+
+  @override
+  List<Object> get props => [];
+}
