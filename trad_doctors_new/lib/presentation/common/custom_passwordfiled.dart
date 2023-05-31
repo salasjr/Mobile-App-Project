b0e2183ea7da6fdd57e6c7b03c4549fc9abf64ca
salahadin juhar <110789948+salasjr@users.noreply.github.com>
2023-06-01 00:25:32 +0300

    Update custom_passwordfiled.dart

--- trad_doctors_new/lib/presentation/common/custom_passwordfiled.dart
+++ trad_doctors_new/lib/presentation/common/custom_passwordfiled.dart
@@ -1,1 +1,60 @@
+import 'package:flutter/material.dart';
+
+class CustomPasswordField extends StatefulWidget {
+  final TextEditingController controller;
+  final String labelText;
+  final String validation;
+
+  const CustomPasswordField({
+    Key? key,
+    required this.controller,
+    required this.labelText,
+    required this.validation,
+  }) : super(key: key);
+
+  @override
+  State<CustomPasswordField> createState() => _CustomPasswordFieldState();
+}
+
+class _CustomPasswordFieldState extends State<CustomPasswordField> {
+  bool _obscureText = true;
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      controller: widget.controller,
+      obscureText: _obscureText,
+      decoration: InputDecoration(
+        labelText: widget.labelText,
+        border: const OutlineInputBorder(
+          // borderRadius: BorderRadius.all(Radius.circular(5)),
+          borderSide: BorderSide(
+            color: Color.fromARGB(255, 143, 143, 143),
+          ),
+        ),
+        enabledBorder: const OutlineInputBorder(
+          // borderRadius: BorderRadius.all(Radius.circular(5)),
+          borderSide: BorderSide(
+            color: Color.fromARGB(255, 143, 143, 143),
+          ),
+        ),
+        suffixIcon: GestureDetector(
+          onTap: () {
+            setState(() {
+              _obscureText = !_obscureText;
+            });
+          },
+          child: Icon(
+            _obscureText ? Icons.visibility : Icons.visibility_off,
+          ),
+        ),
+      ),
+      validator: (val) {
+        if (val == null || val.isEmpty) {
+          return widget.validation;
+        }
+        return null;
+      },
+    );
+  }
+}
 
