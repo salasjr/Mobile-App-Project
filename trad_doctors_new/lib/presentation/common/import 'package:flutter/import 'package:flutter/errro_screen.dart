957ee27a83d14c38515ee1498395f896d135f1af
salahadin juhar <110789948+salasjr@users.noreply.github.com>
2023-06-01 00:30:44 +0300

    Create errro_screen.dart

---
+++ trad_doctors_new/lib/presentation/common/import 'package:flutter/import 'package:flutter/errro_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class ErrorScreen extends StatelessWidget {
+  final Exception? error;
+  const ErrorScreen({super.key, this.error});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Error"),
+      ),
+      body: const Center(
+        child: Text("Page not found"),
+      ),
+    );
+  }
+}
