4ee2372d7a37c196530d70dbd4d144c6c9e6f46a
Betselot Kidane <107115522+betselot49@users.noreply.github.com>
2023-06-03 01:33:25 +0300

    updated global variables

--- trad_doctors_new/lib/presentation/constants/global_variables.dart
+++ trad_doctors_new/lib/presentation/constants/global_variables.dart
@@ -1,18 +1,19 @@
 import 'package:flutter/material.dart';
 
-// String uri = 'http://10.6.223.225:3000';
-// String uri = 'http://192.168.43.111:3000';
+// Use the current ip iddress you are connected.
+// example: 'http://192.168.78.111:3000';
+
 String uri = 'http://192.168.78.111:3000';
-// String uri = 'http://localhost:3000';
+
 
 class GlobalVariables {
   static const backgroundColor = Colors.white;
   static const selectedNavBarColor = Color.fromARGB(255, 0, 153, 133);
-  // static const selectedNavBarColor = Color.fromARGB(255, 24, 0, 142);
+  
   static const secondaryColor = Color.fromARGB(255, 0, 153, 133);
-  // static const secondaryColor = Color.fromARGB(255, 22, 3, 118);
+
   static const greyBackgroundColor = Color.fromARGB(255, 240, 240, 240);
-  // static const primaryColor = Color.fromRGBO(0, 218, 205, 1);
+
   static const primaryColor = Color.fromARGB(255, 0, 153, 133);
 
   static const verticalSizedBox = SizedBox(height: 10);
