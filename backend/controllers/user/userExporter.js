9f6a797576301e4ba796bf0a68f6ee62cbf80c0c
yohannes Desta <108905193+yohannesjoya@users.noreply.github.com>
2023-06-03 01:27:40 +0300

    Delete backend directory

--- backend/controllers/user/userExporter.js
+++
@@ -1,4 +0,0 @@
-const { getUserData } = require("./getuserdata");
-const { getAllDoctors } = require("./getalldoctors");
-
-module.exports = { getUserData, getAllDoctors };
--- backend/routes/profile.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const profileRouter = express.Router();
-const {} = require("../controllers/profile/profileExporter");
-
-// get user data
-profileRouter.get("/---", () => {});
-profileRouter.get("/---", () => {});
-
-module.exports = profileRouter;
