32f702a859330fba461b5331a551be61f18184a5
fikreanteneh <105011492+fikreanteneh@users.noreply.github.com>
2023-06-03 00:57:17 +0300

    Delete trad_doctors_new/lib directory

--- trad_doctors_new/lib/application/bloc/patient_appointement_bloc/patient_appoinetment_bloc_event.dart
+++
@@ -1,8 +0,0 @@
-part of 'patient_appoinetment_bloc.dart';
-
-abstract class PatientAppoinetmentBlocEvent extends Equatable {
-  const PatientAppoinetmentBlocEvent();
-
-  @override
-  List<Object> get props => [];
-}
